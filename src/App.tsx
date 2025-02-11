7612373fb570fe93b0781e4924b735d3da648a78
xulei <xl18845898715@gmail.com>
2025-02-11 17:20:44 +0800

    feat: 添加动画

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useRoutes } from 'react-router-dom';
 import routes from './router';
-import Header from './components/Header';
+import Header from '@/components/Header';
 
 function App() {
   return (
     // bg-[url('/src/assets/images/bg.png')
-    <div className="w-full h-full flex flex-col bg-gray-100 ]">
+    <div className="w-full h-full flex-col  bg-gray-100 ]">
       <Header title="这是一个渐变色的标题" />
       <div>{useRoutes(routes)}</div>
-      {/* <Content /> */}
     </div>
   );
 }
