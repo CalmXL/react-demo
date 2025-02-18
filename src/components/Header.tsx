3397debd8b56736e3735bb549413870e4b7716ec
xulei <xl18845898715@gmail.com>
2025-02-18 16:47:18 +0800

    feat: header 添加标题以及时间

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import Nav from './Nav';
+import Time from './Time';
 
 export default function Header({ title }: { title: string }) {
   return (
     <div className="bg-gray-800 relative">
       <Nav />
+      <h1 className="absolute text-xl top-1/2 left-1/2 text-white -translate-x-1/2 -translate-y-1/2">
+        {title}
+      </h1>
+
+      <Time />
       {/* <div className="absolute right-2 top-0 h-[100%] flex items-center cursor-pointer text-white hover:text-gray-200">
         <i className="iconfont icon-shouce text-xl mr-1"></i>
         运维手册
